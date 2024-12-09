6b468811087b1e1b8ac5552b5f87c19acb7254c2
JRosa <jrosa@berklee.edu>
2024-12-08 20:46:37 -0500

    Background

--- script.py
+++ script.py
@@ -4,14 +4,21 @@ from pygame.locals import *
 
 pygame.init()
 
-screen_width = 612
-screen_height = 382
+screen_width = 1600
+screen_height = 1000
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
 
+#define variables
+tile_size = 50
 #load images
-bg_img = pygame.image.load('img/background.jpeg')
+bg_img = pygame.image.load('background-2.jpeg')
+
+def draw_grid():
+	for line in range(0, 20):
+		pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
+		pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
 
 screen.fill((0,0,0))
 run = True 
@@ -22,6 +29,8 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+            
+        pygame.display.update()
 pygame.quit()
 
 
